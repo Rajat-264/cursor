e78eb87c82c90ec31f840cf56151edbc8d98b27f
Rajat-264 <handerajat@gmail.com>
2025-06-14 16:22:59 +0530

    Hovering effect

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import Cursor from './components/Cursor.jsx';
 import { CursorProvider } from './context/CursorContext.jsx';
-import useCursorHover from './hooks/UseCursorHover.jsx';
+import useCursorHover from './hooks/useCursorHover.jsx';
 
-export { Cursor, CursorProvider, useCursorHover };
+export { 
+  Cursor, 
+  CursorProvider, 
+  useCursorHover 
+};
+
+export default Cursor;
