93ea53b26557c16a99c8dc5433b0b0bce20b0ed0
Rajat-264 <handerajat@gmail.com>
2025-06-14 16:46:45 +0530

    hover effect

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,7 +4,8 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import postcss from 'rollup-plugin-postcss';
-import terser from '@rollup/plugin-terser';
+import { terser } from 'rollup-plugin-terser';
+import copy from 'rollup-plugin-copy'; 
 
 const extensions = ['.js', '.jsx'];
 
@@ -37,6 +38,15 @@ export default {
       extract: true,
       minimize: true,
       modules: false,
+    }),
+    copy({
+      targets: [
+        { 
+          src: 'src/styles/cursor.css', 
+          dest: 'dist/styles',
+          rename: 'cursor.css' 
+        },
+      ],
     }),
     terser(),
   ],
